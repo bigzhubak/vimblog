d49944051a4039232526e34d3a1edc2a2a4faa44
bigzhu <vermiliondun@gmail.com>
2014-04-30 17:54:42 -0400

    加入评论

--- vimblog.py
+++ vimblog.py
@@ -3,12 +3,11 @@
 import tornado.ioloop
 import tornado.web
 
-OK = '0'
-html_path = '/home/bigzhu/Dropbox/knowledge/html/'
+HTML_PATH = '/home/bigzhu/Dropbox/knowledge/html/'
 
 
 def getHtmlContent(name):
-    name_file = open(html_path + name + '.html', 'r')
+    name_file = open(HTML_PATH + name + '.html', 'r')
     content = name_file.read()
     name_file.close()
     return content
@@ -18,7 +17,7 @@ class blog(tornado.web.RequestHandler):
     def get(self, name):
         content = getHtmlContent(name)
         print content
-        self.render("./template/detail.html", title='test', content=content)
+        self.render("./template/detail.html", title=name, content=content)
 
 
 class MyStaticFileHandler(tornado.web.StaticFileHandler):
@@ -28,9 +27,10 @@ class MyStaticFileHandler(tornado.web.StaticFileHandler):
         self.set_header('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0')
 
 
-settings = {'debug': True, 'cookie_secret': 'bigzhu so big', 'autoescape':None}
+settings = {'debug': True, 'cookie_secret': 'bigzhu so big', 'autoescape': None}
 url_map = [
     (r'/blog/(.*)', blog),
+    (r'/static/(.*)', MyStaticFileHandler, {'path': "./static"})
 ]
 application = tornado.web.Application(url_map, **settings)
 
