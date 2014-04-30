3ed32ab8781264dcaf1839fb9c5468d4ec16507a
bigzhu <vermiliondun@gmail.com>
2014-04-30 15:32:43 -0400

    显示读取 vimwiki 的html内容

--- vimblog.py
+++ vimblog.py
@@ -4,12 +4,22 @@ import tornado.ioloop
 import tornado.web
 import public
 
+
 OK = '0'
+html_path = '/home/bigzhu/Dropbox/knowledge/html/'
+
+
+def getHtmlContent(name):
+    name_file = open(html_path + name + '.html', 'r')
+    content = str(name_file.read())
+    name_file.close()
+    return content
 
 
 class blog(tornado.web.RequestHandler):
-    def get(self):
-        self.write("Hello, world, haha")
+    def get(self, name):
+        content = getHtmlContent(name)
+        self.render("./template/detail.html", title='test', content=content)
 
 
 class MyStaticFileHandler(tornado.web.StaticFileHandler):
@@ -19,14 +29,10 @@ class MyStaticFileHandler(tornado.web.StaticFileHandler):
         self.set_header('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0')
 
 
-the_globals = globals().copy()
-url_map = public.getURLMap(the_globals)
-
-settings = {'debug': True,
-            'cookie_secret': 'bigzhu so big',
-            'login_url': "/static/login.html"}
-url_map.append((r'/static/(.*)', MyStaticFileHandler, {'path': "./static"}))
-
+settings = {'debug': True, 'cookie_secret': 'bigzhu so big'}
+url_map = [
+    (r'/blog/(.*)', blog),
+]
 application = tornado.web.Application(url_map, **settings)
 
 if __name__ == "__main__":
