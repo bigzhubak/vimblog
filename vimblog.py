7cd7c00ffd65b99026fde65c90259f612d80a550
bigzhu <vermiliondun@gmail.com>
2014-09-28 12:48:45 +0800

    改用 jinja2 来做模板

--- vimblog.py
+++ vimblog.py
@@ -1,19 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import os
 import tornado.ioloop
 import tornado.web
 import json
+import time
 from search_vimwiki import SearchWiki
 
 from os.path import expanduser
+import tornado_bz
+import public_bz
+
+import jinja2_bz
+
 home = expanduser("~")
 
-HTML_PATH = home+'/Dropbox/knowledge/html/'
-WIKI_PATH = home+'/Dropbox/knowledge/data/'
-CLICK_COUNT = home+'/click_count'
-#click_path = './click/'
-click_path = home+'/click/'
+HTML_PATH = home + '/Dropbox/knowledge/html/'
+WIKI_PATH = home + '/Dropbox/knowledge/data/'
+CLICK_COUNT = home + '/click_count'
+click_path = home + '/click/'
 
 SITE = 'site'
 key_names = {}
@@ -32,30 +36,36 @@ def popSome():
 
 def getKeyNames():
     try:
-        f = open('./key_name', 'r')
+        f = open('./key_name', 'a+')
         global key_names
         global key_names_sorted
-        key_names = json.loads(f.read())
-        popSome()
-        key_names_sorted = sorted(key_names.items(), key=lambda by: by[1], reverse=True)
+        content = f.read()
+        if content != '':
+            key_names = json.loads(content)
+            popSome()
+            key_names_sorted = sorted(
+                key_names.items(),
+                key=lambda by: by[1],
+                reverse=True)
         f.close()
     except IOError:
-        pass
+        print public_bz.getExpInfoAll()
 
 
 def getClickCount():
     try:
-        f = open(CLICK_COUNT, 'r')
+        f = open(CLICK_COUNT, 'a+')
         global click_count
-        click_count = json.loads(f.read())
+        content = f.read()
+        if content != '':
+            click_count = json.loads(content)
         f.close()
     except IOError:
-        print 'IOError'
-        pass
+        print public_bz.getExpInfoAll()
 
 
 def save(file_name, content):
-    f = open(file_name, 'w')
+    f = open(file_name, 'w+')
     print >>f, json.dumps(content)
     f.close()
 
@@ -81,13 +91,16 @@ def increase(dic, name):
 
 def refreshKeyNamesCount(name, count):
     global key_names
-    #if key_names[name] % 5 == 0:
+    # if key_names[name] % 5 == 0:
     if count != 0:
         key_names[name] = count
         saveKeyNames()
-    #需要排序
+    # 需要排序
     global key_names_sorted
-    key_names_sorted = sorted(key_names.items(), key=lambda by: by[1], reverse=True)
+    key_names_sorted = sorted(
+        key_names.items(),
+        key=lambda by: by[1],
+        reverse=True)
 
 
 def addClickCount(name):
@@ -122,6 +135,7 @@ def getHtmlContent(name):
         name_file.close()
         return content
     except IOError:
+        print public_bz.getExpInfoAll()
         return '0'
 
 
@@ -133,6 +147,11 @@ def getLen(lists):
 
 
 class list(tornado.web.RequestHandler):
+
+    '''
+    显示 blog 列表
+    '''
+
     def get(self, name='*'):
         lists = getList(str(name))
         if name == '*':
@@ -143,13 +162,23 @@ class list(tornado.web.RequestHandler):
         global key_names
         global key_names_sorted
         global click_count
-        self.render("./template/list.html", title=title, lists=lists, key_names=key_names_sorted, click_count=click_count)
+
+        t = jinja2_bz.getTemplate(self.__class__.__name__)
+        html = t.render(
+            title=title,
+            lists=lists,
+            key_names=key_names_sorted,
+            click_count=click_count,
+            time=time)
+        self.write(html)
 
 
 class blog(tornado.web.RequestHandler):
+
     '''
     显示 blog 的详细内容
     '''
+
     def get(self, name):
         if name is None:
             name = 'index'
@@ -161,18 +190,25 @@ class blog(tornado.web.RequestHandler):
         global key_names_sorted
         global new_key_names
         count = addClickCount(name)
-        self.render("./template/detail.html", title=name, content=content, key_names=key_names_sorted, new_key_names=new_key_names, count=count)
+
+        t = jinja2_bz.getTemplate(self.__class__.__name__)
+        html = t.render(
+            title=name,
+            content=content,
+            key_names=key_names_sorted,
+            new_key_names=new_key_names,
+            count=count)
+        self.write(html)
 
 
-
-settings = {'debug': True, 'cookie_secret': 'bigzhu so big', 'autoescape': None, "static_path": os.path.join(os.path.dirname(__file__), "static"), }
 url_map = [
     (r'/', list),
     (r'/blog/(.*)', blog),
     (r'/list/(.*)', list),
     (r'/(.*)', blog),
 ]
-application = tornado.web.Application(url_map, **settings)
+
+application = tornado_bz.getApplication(url_map)
 
 if __name__ == "__main__":
     getKeyNames()
